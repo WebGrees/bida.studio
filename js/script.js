4e59bb88edbc114fabecf2662d5e50bf4e596948
ambroseus <ambroseus@gmail.com>
2020-05-23 08:20:09 +0300

    fixed focusout click conflict

--- js/script.js
+++ js/script.js
@@ -10,16 +10,21 @@ $('.header__menu-btn').on('click', function(e) {
 
 /*Dropdown Menu*/
 $('.dropdown').click(function () {
-        $(this).attr('tabindex', 1).focus();
-        $(this).toggleClass('active');
-        $(this).find('.dropdown-menu').slideToggle(1000);
-    });
-    $('.dropdown').focusout(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).addClass('active');
+    $(this).find('.dropdown-menu').slideToggle(1000);
+});
+
+// https://stackoverflow.com/questions/13980448/jquery-focusout-click-conflict
+$('.dropdown').focusout(function () {
+    window.setTimeout(function () {
         $(this).removeClass('active');
         $(this).find('.dropdown-menu').slideUp(300);
-    });
-    $('.dropdown .dropdown-menu li').click(function () {
-        $(this).parents('.dropdown').find('span').text($(this).text());
-        $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
-    });
+    }, 100)
+});
+
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
 /*End Dropdown Menu*/
